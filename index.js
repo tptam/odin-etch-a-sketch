bd3e5fd7230d1b2d550756b3282bd1978e1eaf4f
tptam <99264397+tptam@users.noreply.github.com>
2024-06-29 03:12:14 +0900

    Add function to reset the number of squares

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-
 const container = document.querySelector("#container");
 
 for (let i = 0; i < 16; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
+}
 
-    container.addEventListener(
-        "mouseenter",
-        (e) => {
-            if (e.target.classList.contains("square")) {
-                e.target.style.backgroundColor = "pink";
-            }
-        },
-        true
-    )
-    container.addEventListener(
-        "mouseleave",
-        (e) => {
-            if (e.target.classList.contains("square")) {
-                e.target.style.backgroundColor = "white";
-            }
-        },
-        true
-    )
+container.addEventListener(
+    "mouseenter",
+    (e) => {
+        if (e.target.classList.contains("square")) {
+            e.target.style.backgroundColor = "pink";
+        }
+    },
+    true
+)
+container.addEventListener(
+    "mouseleave",
+    (e) => {
+        if (e.target.classList.contains("square")) {
+            e.target.style.backgroundColor = "white";
+        }
+    },
+    true
+)
+
+function initScreen(n){
+    const container = document.querySelector("#container");
+    container.style.width = "80vmin";
+    const squareSize = `${80 / n}vmin`;
+    container.textContent = "";
+    for (let i = 0; i < n**2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = squareSize;
+        square.style.height = squareSize;
+        container.appendChild(square);
+    }
 }
